da2e26245f4245c25e5938ec5f386d475766ba4d
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-08-27 16:33:54 -0300

    Update GetResourceContent.vue

--- src/Assets/js/components/custom/GetResourceContent.vue
+++ src/Assets/js/components/custom/GetResourceContent.vue
@@ -1,45 +1,48 @@
 <template>
     <tr>
-        <template v-for="(item,i) in cols">
-            <td :key="i" v-if="loading">
-                <div
-                    class="shimmer"
-                    :style="{
+        <template v-if="loading">
+            <template v-for="(item,i) in cols">
+                <td :key="i">
+                    <div
+                        class="shimmer"
+                        :style="{
                             width : w ? w : '100%',
-                            height : h ? h : '100%'
+                            height : (Math.random() * (45 - 10) + 20)
                         }"
-                />
-            </td>
+                    />
+                </td>
+            </template>
         </template>
-        <template v-for="(key,i) in Object.keys(content)">
-            <td :key="i" v-if="!loading">
-                <div class="d-flex flex-column">
-                    <template v-if="i ==0">
-                        <b>
-                            <a
-                                :href="`{${resource_route}/${row_code}`"
-                                class="link"
-                                v-html="content[key]"
-                            />
-                        </b>
-                    </template>
-                    <template v-else>
-                        <div v-html="content[key]" />
-                    </template>
-
-                    <resource-crud-buttons
-                        v-if="i==0"
-                        :data="{
+        <template v-else>
+            <template v-for="(key,i) in Object.keys(content)">
+                <td :key="i">
+                    <div class="d-flex flex-column">
+                        <template v-if="i ==0">
+                            <b>
+                                <a
+                                    :href="`${resource_route}/${row_code}`"
+                                    class="link"
+                                    v-html="content[key]"
+                                />
+                            </b>
+                        </template>
+                        <template v-else>
+                            <div v-html="content[key]" />
+                        </template>
+                        <resource-crud-buttons
+                            v-if="i==0"
+                            :data="{
                         code : row_code,
                         route : `${resource_route}/${row_code}`,
                         can_view : can_view,
                         can_update : can_update,
                         can_delete : can_delete
                     }"
-                        :id="row_id"
-                    />
-                </div>
-            </td>
+                            :id="row_id"
+                        />
+                    </div>
+                </td>
+            </template>
         </template>
     </tr>
 </template>
@@ -103,34 +106,14 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-@-webkit-keyframes placeholderShimmer {
-    0% {
-        background-position: -468px 0;
-    }
-
-    100% {
-        background-position: 468px 0;
-    }
-}
-
 .shimmer {
-    background: #f6f7f8;
-    background-image: linear-gradient(
-        to right,
-        #f6f7f8 0%,
-        #edeef1 20%,
-        #f6f7f8 40%,
-        #f6f7f8 100%
-    );
-    background-repeat: no-repeat;
-    background-size: 800px "100%";
-    display: inline-block;
-    position: relative;
-
-    -webkit-animation-duration: 1s;
-    -webkit-animation-fill-mode: forwards;
-    -webkit-animation-iteration-count: infinite;
-    -webkit-animation-name: placeholderShimmer;
-    -webkit-animation-timing-function: linear;
+    background: linear-gradient(267deg, #e9ecef, #dcdcdc, #ffffff, #dcdcdc);
+    background-size: 400% 400%;
+    -webkit-animation: animationGradient 3s ease infinite;
+    -moz-animation: animationGradient 3s ease infinite;
+    animation: animationGradient 3s ease infinite;
+    background-color: #e5e5e5;
+    height: 100%;
+    width: 100%;
 }
 </style>
