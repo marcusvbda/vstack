605f5771363486895b13d2702039aa28fd890dc9
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-09-10 17:38:45 -0300

    Update text.blade.php

--- src/Views/resources/filter/text.blade.php
+++ src/Views/resources/filter/text.blade.php
@@ -1,8 +1,21 @@
-<el-input 
-    clearable size='medium' 
-    v-model='{{"filter.$index"}}' 
+@if(!@$mask) 
+    <el-input 
+        clearable size='medium' 
+        v-model='{{"filter.$index"}}' 
+        @change='makeNewRoute'
+        type='text' class='w-100'      
+        placeholder='{{$placeholder}}'
+        v-bind:class='{"withFilter": {{"filter.$index"}}}'>
+    </el-input>
+@else
+<div  class="w-100 el-input el-input--medium el-input--suffix" v-bind:class='{"withFilter": {{"filter.$index"}}}'>
+    <input type="text" 
+    autocomplete="off" 
+    v-model='{{"filter.$index"}}'
     @change='makeNewRoute'
-    type='text' class='w-100'      
+    type='text'
     placeholder='{{$placeholder}}'
-    v-bind:class='{"withFilter": {{"filter.$index"}}}'>
-</el-input>
+    v-mask='{{json_encode($mask)}}'
+        class="el-input__inner">
+</div>
+@endif
