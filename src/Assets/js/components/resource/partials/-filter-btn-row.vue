69bfd390c83b151410070bd233755206a13b56ee
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-09-23 11:03:00 -0300

    Update -filter-btn-row.vue

--- src/Assets/js/components/resource/partials/-filter-btn-row.vue
+++ src/Assets/js/components/resource/partials/-filter-btn-row.vue
@@ -1,18 +1,27 @@
 <template>
     <tbody>
-        <tr v-if="f.label">
-            <td class="px-2 tr-label" style="position: relative;" @click="visible = !visible">
+        <tr v-if="f.label" class="tr-hover">
+            <td
+                class="px-2 tr-label"
+                style="position: relative"
+                @click="visible = !visible"
+            >
                 <span
                     class="badge-number"
-                    v-if="$root.$refs.tags_filter.hasContent(filter,index)"
+                    v-if="$root.$refs.tags_filter.hasContent(filter, index)"
                     v-html="'!'"
                 />
                 <div class="w-100">
-                    <div class="d-flex flex-row justify-content-between">
+                    <div
+                        class="d-flex flex-row justify-content-between px-3 font-weight-bold"
+                    >
                         <label
                             class="mb-0 text-muted"
-                            style="font-size:13px;font-weight:bold;"
+                            style="font-size: 13px; font-weight: bold"
                             v-html="f.label ? f.label : k"
+                        />
+                        <span
+                            :class="`el-icon-arrow-${!visible ? 'down' : 'up'}`"
                         />
                     </div>
                 </div>
@@ -59,14 +68,13 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.tr-label {
-    .w-100 {
-        opacity: 0.7;
-        transition: 0.4s;
-        cursor: pointer;
-        &:hover {
-            opacity: 1;
-        }
+.tr-hover {
+    opacity: 0.7;
+    transition: 0.4s;
+    cursor: pointer;
+    &:hover {
+        opacity: 1;
+        background-color: #ececec;
     }
 }
 </style>
