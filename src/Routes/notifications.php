73091f96a455a95994c889f4059ed9dbb9ce7e94
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-10-01 14:03:22 -0300

    Update notifications.php

--- src/Routes/notifications.php
+++ src/Routes/notifications.php
@@ -1,10 +1,13 @@
 <?php
+
+use marcusvbda\vstack\Controllers\NotificationsController;
+
 Route::group(['prefix' => "admin"], function () {
     Route::group(['prefix' => "vstack"], function () {
         Route::group(['middleware' => ['web', 'auth']], function () {
             Route::group(['prefix' => "notifications"], function () {
-                Route::post('{user}', '\marcusvbda\vstack\Controllers\NotificationsController@get')->middleware(['hashids:user', 'bindings'])->name("notifications.get");
-                Route::delete('{user}/{id}/destroy', '\marcusvbda\vstack\Controllers\NotificationsController@destroy')->middleware(['hashids:user', 'bindings'])->name("notifications.destroy");
+                Route::post('{user}', [NotificationsController::class, 'get'])->middleware(['hashids:user', 'bindings'])->name("notifications.get");
+                Route::delete('{user}/{id}/destroy', [NotificationsController::class, 'destroy'])->middleware(['hashids:user', 'bindings'])->name("notifications.destroy");
             });
         });
     });
