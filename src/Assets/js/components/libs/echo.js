8e4303783b2597a1dd3f9b95d54c4f7d0304ad85
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-12-03 14:58:42 -0300

    Update echo.js

--- src/Assets/js/components/libs/echo.js
+++ src/Assets/js/components/libs/echo.js
@@ -1,11 +1,13 @@
 import Echo from 'laravel-echo'
+import Vue from 'vue'
 if (laravel.chat) {
-    if (laravel.chat.pusher_key) {
-        window.Pusher = require('pusher-js')
-        window.Echo = new Echo({
-            broadcaster: 'pusher',
-            key: laravel.chat.pusher_key,
-            cluster: laravel.chat.pusher_cluster
-        })
-    }
+	if (laravel.chat.pusher_key) {
+		window.Pusher = require('pusher-js')
+		window.Echo = new Echo({
+			broadcaster: 'pusher',
+			key: laravel.chat.pusher_key,
+			cluster: laravel.chat.pusher_cluster
+		})
+		Vue.prototype.$echo = window.Echo
+	}
 }
