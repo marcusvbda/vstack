6292a627f069dd9ee498ca9c2b9ba5d0b0e62be7
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-12-04 18:54:05 -0300

    Update CascadeOrRestrictSoftdeletes.php

--- src/Traits/CascadeOrRestrictSoftdeletes.php
+++ src/Traits/CascadeOrRestrictSoftdeletes.php
@@ -4,25 +4,26 @@ namespace marcusvbda\vstack\Traits;
 
 trait CascadeOrRestrictSoftdeletes
 {
-    protected static function bootCascadeOrRestrictSoftdeletes()
-    {
-        static::deleting(function ($model) {
-            $model->validateCascadeSoftdeletes($model);
-            $model->validateRestrictSoftdeletes($model);
-        });
-    }
+	protected static function bootCascadeOrRestrictSoftdeletes()
+	{
+		static::deleting(function ($model) {
+			$model->validateCascadeSoftdeletes($model);
+			$model->validateRestrictSoftdeletes($model);
+		});
+	}
 
-    private function validateCascadeSoftdeletes($model)
-    {
-        $relations = $model->cascadeDeletes;
-        foreach ($relations as $relation) $model->{$relation}()->delete();
-    }
+	private function validateCascadeSoftdeletes($model)
+	{
+		$relations = $model->cascadeDeletes;
+		
+		foreach ($relations as $relation) $model->{$relation}()->delete();
+	}
 
-    private function validateRestrictSoftdeletes($model)
-    {
-        $relations = $model->restrictDeletes;
-        foreach ($relations as $relation) {
-            if ($model->{$relation}->count() >= 1) abort(500, "Não pode ser excluido pois está em uso");
-        }
-    }
+	private function validateRestrictSoftdeletes($model)
+	{
+		$relations = $model->restrictDeletes;
+		foreach ($relations as $relation) {
+			if ($model->{$relation}) abort(500, "Não pode ser excluido pois está em uso");
+		}
+	}
 }
