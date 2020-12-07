a5887e390d8ab7a8eb2202128c306ebbc6fae68d
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-12-07 18:38:59 -0300

    Update ResourceCrudButtons.vue

--- src/Assets/js/components/resource/ResourceCrudButtons.vue
+++ src/Assets/js/components/resource/ResourceCrudButtons.vue
@@ -1,19 +1,16 @@
 <template>
-    <div class="d-flex flex-row flex-wrap cursor-pointer align-items-center" style="min-height: 25px">
-        <el-dropdown trigger="click" :class="`ml-4 ${!isOpened ? '' : ''}`" @visible-change="setVisible">
-            <span class="el-dropdown-link" style="color: #5a8dee !important">Ações<i class="el-icon-arrow-down el-icon--right"></i> </span>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item icon="el-icon-view" v-if="data.can_view">
-                    <a class="link ml-3" :href="data.route">Visualizar </a>
-                </el-dropdown-item>
-                <el-dropdown-item icon="el-icon-edit" v-if="data.can_update">
-                    <a class="link ml-3" v-if="data.can_update" :href="`${data.route}/edit`"> Editar </a>
-                </el-dropdown-item>
-                <el-dropdown-item icon="el-icon-delete  text-danger" divided v-if="data.can_delete">
-                    <a class="link ml-3" v-if="data.can_delete" @click.prevent="destroy" style="color: red !important"> Excluir </a>
-                </el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+    <div class="d-flex flex-row flex-wrap cursor-pointer align-items-center justify-content-center" style="min-height: 25px">
+        <el-button-group>
+            <el-tooltip class="item" effect="dark" content="Ver Detalhes" placement="top" v-if="data.can_view">
+                <el-button size="small" plain type="info" icon="el-icon-search" @click="goTo(data.route)" />
+            </el-tooltip>
+            <el-tooltip class="item" effect="dark" content="Editar Registro" placement="top" v-if="data.can_update">
+                <el-button size="small" plain type="primary" icon="el-icon-edit" @click="goTo(`${data.route}/edit`)" />
+            </el-tooltip>
+            <el-tooltip class="item" effect="dark" content="Editar Registro" placement="top" v-if="data.can_delete">
+                <el-button size="small" plain type="danger" icon="el-icon-delete" @click.prevent="destroy" />
+            </el-tooltip>
+        </el-button-group>
     </div>
 </template>
 <script>
@@ -21,13 +18,12 @@ export default {
     props: ['data', 'id'],
     data() {
         return {
-            isOpened: false,
             loading: null,
         }
     },
     methods: {
-        setVisible(val) {
-            this.isOpened = val
+        goTo(route) {
+            window.location.href = route
         },
         destroy() {
             this.$confirm(`Confirma Exclusão ?`, 'Confirmação', {
