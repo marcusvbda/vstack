198c850068173852e2628117b962cd9a86ca370a
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2020-12-11 16:57:36 -0300

    Update DefaultModel.php

--- src/Models/DefaultModel.php
+++ src/Models/DefaultModel.php
@@ -8,24 +8,43 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use marcusvbda\vstack\Traits\CascadeOrRestrictSoftdeletes;
 use marcusvbda\vstack\Models\Scopes\TenantScope;
 use marcusvbda\vstack\Models\Observers\TenantObserver;
+use Carbon\Carbon;
 
 class DefaultModel extends Model
 {
-    use hasCode, SoftDeletes, CascadeOrRestrictSoftdeletes;
-    public $guarded = ["created_at"];
-    public $cascadeDeletes = [];
-    public $restrictDeletes = [];
-    public static function boot()
-    {
-        parent::boot();
-        if (static::hasTenant()) {
-            static::observe(new TenantObserver());
-            static::addGlobalScope(new TenantScope());
-        }
-    }
+	use hasCode, SoftDeletes, CascadeOrRestrictSoftdeletes;
+	public $guarded = ["created_at"];
+	public $cascadeDeletes = [];
+	public $restrictDeletes = [];
+	public static function boot()
+	{
+		parent::boot();
+		if (static::hasTenant()) {
+			static::observe(new TenantObserver());
+			static::addGlobalScope(new TenantScope());
+		}
+	}
 
-    public static function hasTenant()
-    {
-        return true;
-    }
+	public function getCreatedAtAttribute($value)
+	{
+		$timezone = config("app.timezone");
+		return Carbon::parse($value, "UTC")->tz($timezone);
+	}
+
+	public function getUpdatedAtAttribute($value)
+	{
+		$timezone = config("app.timezone");
+		return Carbon::parse($value, "UTC")->tz($timezone);
+	}
+
+	public function getDeletedAtAttribute($value)
+	{
+		$timezone = config("app.timezone");
+		return Carbon::parse($value, "UTC")->tz($timezone);
+	}
+
+	public static function hasTenant()
+	{
+		return true;
+	}
 }
