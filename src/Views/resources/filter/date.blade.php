72eeafc285f1906f814e083b2c91dd648024a3a3
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-01-07 07:43:41 -0300

    Update date.blade.php

--- src/Views/resources/filter/date.blade.php
+++ src/Views/resources/filter/date.blade.php
@@ -1,11 +1,3 @@
-<el-date-picker 
-    size='medium' 
-    class='w-100' 
-    clearable
-    v-model='{{"filter.$index"}}' 
-    @change='makeNewRoute' 
-    type='date'
-    :format="'dd/MM/yyyy'"
-    value-format="'yyyy-MM-dd'"
-    placeholder='{{$placeholder}}'>
+<el-date-picker size='medium' class='w-100' clearable v-model='{{"filter.$index"}}' @change='makeNewRoute'
+    type='date' :format="'dd/MM/yyyy'" :value-format="'{{ $value_format }}'" placeholder='{{$placeholder}}'>
 </el-date-picker>
