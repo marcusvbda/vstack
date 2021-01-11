935d4e624d7a637037b1ae7d2e190f245c63674f
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-01-11 10:38:41 -0300

    Update ResourceExportBtn.vue

--- src/Assets/js/components/resource/ResourceExportBtn.vue
+++ src/Assets/js/components/resource/ResourceExportBtn.vue
@@ -1,64 +1,100 @@
 <template>
-    <div class="d-flex flex-row flex-wrap cursor-pointer align-items-center" style="min-height: 25px">
-        <div class="d-flex align-items-center mr-2">
-            <span style="font-weight: 600; opacity: 0.4; letter-spacing: -1">ID .: {{ Number(id).pad(8) }}</span>
-        </div>
-        <el-dropdown trigger="click" :class="`ml-4 ${!isOpened ? 'crud-btns' : ''}`" @visible-change="setVisible">
-            <span class="el-dropdown-link" style="color: #5a8dee !important">Ações<i class="el-icon-arrow-down el-icon--right"></i> </span>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item v-if="data.can_view" style="padding: 0">
-                    <a class="link" :href="data.route" style="padding: 0 40px"> <span class="el-icon-view mr-3" />Visualizar </a>
-                </el-dropdown-item>
-                <el-dropdown-item v-if="data.can_update" style="padding: 0">
-                    <a class="link" :href="`${data.route}/edit`" style="padding: 0 40px"> <span class="el-icon-edit mr-3" /> Editar </a>
-                </el-dropdown-item>
-                <el-dropdown-item v-if="data.can_delete" style="padding: 0; border-top: 1px solid #e9e9e9; margin-top: 10px">
-                    <a class="link" @click.prevent="destroy" style="color: red !important; padding: 0 40px">
-                        <span class="el-icon-delete text-danger mr-3" />
-                        Excluir
-                    </a>
-                </el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+    <div>
+        <a href="#" @click.prevent="openExportModal">
+            <slot />
+        </a>
+        <el-dialog :title="`Exportação de planilha de ${label}`" :visible.sync="visible" center append-to-body>
+            <div class="row d-flex justify-content-center">
+                <div class="d-flex flex-row align-items-end">
+                    <b>Selecione as colunas que deseja importar em sua planilha</b>
+                    <small class="ml-2 text-muted">Será respeitado o filtro da listagem</small>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-12">
+                    <el-checkbox
+                        class="no-check-mgt mx-1"
+                        v-for="(f, i) in columns"
+                        :key="i"
+                        v-model="columns[i].enabled"
+                        :label="columns[i].label"
+                        @change="saveColumnLocalStorage(i)"
+                        border
+                    />
+                </div>
+            </div>
+            <el-alert
+                class="mt-4"
+                show-icon
+                :title="`Esta importação terá mais de ${limit} resultados e por isso o arquivo será enviado ao seu email após a geração !!`"
+                type="warning"
+                :closable="false"
+                v-if="qty_results > limit"
+            />
+            <span slot="footer" class="dialog-footer d-flex flex-row justify-content-end">
+                <el-button @click="visible = false">Cancelar</el-button>
+                <el-button type="primary" @click="confirm">Exportar Planilha</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 <script>
 export default {
-    props: ['data', 'id'],
+    props: ['id', 'export_columns', 'label', 'qty_results', 'get_params', 'limit'],
     data() {
         return {
-            isOpened: false,
-            loading: null,
+            visible: false,
+            columns: {},
         }
     },
+    mounted() {
+        this.setColumns()
+    },
     methods: {
-        setVisible(val) {
-            this.isOpened = val
+        saveColumnLocalStorage(key) {
+            setTimeout(() => {
+                localStorage.setItem(`${this.id}_export_column_option_${key}`, this.columns[key].enabled)
+            }, 500)
         },
-        destroy() {
-            this.$confirm(`Confirma Exclusão ?`, 'Confirmação', {
-                confirmButtonText: 'Sim',
-                cancelButtonText: 'Não',
-                type: 'error',
-            })
-                .then(() => {
-                    this.loading = this.$loading()
-                    this.$http
-                        .delete(this.data.route + '/destroy', {})
-                        .then((res) => {
-                            res = res.data
-                            return (window.location.href = res.route)
-                        })
-                        .catch((er) => {
-                            this.loading.close()
-                            this.$message({
-                                message: er.response.data.message,
-                                type: 'error',
-                            })
-                        })
+        setColumns() {
+            Object.keys(this.export_columns).map((key) => {
+                let storage_value = localStorage.getItem(`${this.id}_export_column_option_${key}`)
+                this.$nextTick(() => {
+                    this.$set(this.columns, key, {
+                        enabled: storage_value ? storage_value == 'true' : true,
+                        label: this.export_columns[key].label ? this.export_columns[key].label : this.export_columns[key],
+                    })
                 })
-                .catch(() => false)
+            })
+        },
+        openExportModal() {
+            this.visible = true
+        },
+        confirm() {
+            this.$confirm('Exportar relatório para planilha ? ', 'Confirmação').then(() => {
+                const loading = this.$loading({ text: `Aguarde, Gerando Planilha de ${this.id} ...` })
+                this.$http
+                    .post(`/admin/${this.id}/export`, {
+                        get_params: this.get_params,
+                        columns: this.columns,
+                    })
+                    .then((resp) => {
+                        resp = resp.data
+                        this.$message({ type: resp.message_type, message: resp.message, duration: 9999999999, showClose: true })
+                        this.visible = false
+                        this.setColumns()
+                        loading.close()
+                        if (resp.url) return window.open(resp.url, '_BLANK')
+                    })
+            })
         },
     },
 }
 </script>
+<style lang="scss" >
+.no-check-mgt {
+    .el-checkbox__input {
+        margin-top: unset !important;
+    }
+}
+</style>
