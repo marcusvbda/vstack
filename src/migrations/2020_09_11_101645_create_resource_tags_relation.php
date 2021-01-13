c9e7fc6bd5f9cd750ae77db71eff4b7ef32f221c
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-01-13 16:00:46 -0300

    Update 2020_09_11_101645_create_resource_tags_relation.php

--- src/migrations/2020_09_11_101645_create_resource_tags_relation.php
+++ src/migrations/2020_09_11_101645_create_resource_tags_relation.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateResourceTagsRelation extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('resource_tags_relation', function (Blueprint $table) {
-            $table->unsignedBigInteger('resource_tag_id');
-            $table->string('model');
-            $table->foreign('resource_tag_id')
-                ->references('id')
-                ->on('resource_tags');
-            $table->integer('relation_id');
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('resource_tags_relation', function (Blueprint $table) {
+			$table->unsignedBigInteger('resource_tag_id');
+			$table->string('model');
+			$table->foreign('resource_tag_id')
+				->references('id')
+				->on('resource_tags');
+			$table->integer('relation_id');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('resource_tags');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('resource_tags_relation');
+	}
 }
