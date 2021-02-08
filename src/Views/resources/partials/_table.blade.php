03e29af589b35867fe3a282586663ca3d065517c
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-02-08 19:33:22 -0300

    Update _table.blade.php

--- src/Views/resources/partials/_table.blade.php
+++ src/Views/resources/partials/_table.blade.php
@@ -10,7 +10,7 @@ $list_type = \marcusvbda\vstack\Models\ResourceConfig::where("data->user_id", $u
 $list_types = @$resource->listType() ? $resource->listType() : ["table"];
 $order_by = @$_GET["order_by"];
 $order_type = @$_GET["order_type"]=="desc" ? "asc" : "desc";
-$session_list_type = $list_type = @$list_type->data->type ? $list_type->data->type : $list_types[0];
+$session_list_type = @$list_type->data->type ? $list_type->data->type : $list_types[0];
 if(in_array($session_list_type,$list_types)) $list_type = $session_list_type;
 $table = $resource->table();
 $table_keys = array_keys($table);
