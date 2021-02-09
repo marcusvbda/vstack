a48cf408dd6269cac249b220522b244ad60d0d0a
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-02-09 11:43:03 -0300

    Update ResourceCrudButtons.vue

--- src/Assets/js/components/resource/ResourceCrudButtons.vue
+++ src/Assets/js/components/resource/ResourceCrudButtons.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="d-flex flex-row flex-wrap cursor-pointer align-items-center justify-content-center" style="min-height: 25px">
         <el-button-group>
-            <el-tooltip class="item" effect="dark" content="Ver Detalhes" placement="top" v-if="data.can_view">
+            <el-tooltip class="item" effect="dark" content="Visualizar" placement="top" v-if="data.can_view">
                 <el-button size="small" plain type="info" icon="el-icon-search" @click="goTo(data.route)" />
             </el-tooltip>
-            <el-tooltip class="item" effect="dark" content="Editar Registro" placement="top" v-if="data.can_update">
+            <el-tooltip class="item" effect="dark" content="Editar" placement="top" v-if="data.can_update">
                 <el-button size="small" plain type="primary" icon="el-icon-edit" @click="goTo(`${data.route}/edit`)" />
             </el-tooltip>
-            <el-tooltip class="item" effect="dark" content="Editar Registro" placement="top" v-if="data.can_delete">
+            <el-tooltip class="item" effect="dark" content="Excluir" placement="top" v-if="data.can_delete">
                 <el-button size="small" plain type="danger" icon="el-icon-delete" @click.prevent="destroy" />
             </el-tooltip>
         </el-button-group>
