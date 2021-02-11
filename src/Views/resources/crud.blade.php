150d90ab5ed4925f5265caa349c04b0c65d5a49d
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-02-11 17:57:16 -0300

    Update crud.blade.php

--- src/Views/resources/crud.blade.php
+++ src/Views/resources/crud.blade.php
@@ -72,6 +72,16 @@ for($i=0;$i<count($cards);$i++)
 </div>
 @endsection
 @section('content')
+<div class="row mt-2">
+    <div class="col-12">
+        <div class="d-flex flex-row justify-content-between mb-3">
+            <h4>@if( @$resource->icon() ) <span class="{{$resource->icon()}} mr-2"></span> @endif {{$data["page_type"]}} de {{$resource->singularLabel()}}</h4>
+        </div>
+    </div>
+</div>
+@if(($data["page_type"]=="Edição") && $resource->useTags())
+	<resource-tags-input resource='{{$resource->id}}' resource_code='{{@$content->code}}'></resource-tags-input>
+@endif
 @if(@!$data->id)
     @if(@$resource->beforeCreateSlot())
         {!! @$resource->beforeCreateSlot() !!}
@@ -81,22 +91,12 @@ for($i=0;$i<count($cards);$i++)
     {!! @$resource->beforeEditSlot() !!}
     @endif
 @endif
-<div class="row mt-2">
-    <div class="col-12">
-        <div class="d-flex flex-row justify-content-between mb-3">
-            <h4>@if( @$resource->icon() ) <span class="{{$resource->icon()}} mr-2"></span> @endif {{$data["page_type"]}} de {{$resource->singularLabel()}}</h4>
-        </div>
-    </div>
-</div>
-@if(($data["page_type"]=="Edição") && $resource->useTags())
-<resource-tags-input resource='{{$resource->id}}' resource_code='{{@$content->code}}'></resource-tags-input>
-@endif
 <resource-crud 
     :data="{{json_encode($data)}}"  
     :params="{{json_encode($params)}}"  
     redirect="{{$current_route}}" 
     :breadcrumb="{{json_encode($routes)}}" >
-    @if(@!$data->id)
+    @if(@!$data->id && !@$data["id"])
         @if(@$resource->afterCreateSlot())
             <template slot="aftercreate">
                 {!! @$resource->afterCreateSlot() !!}
