6c58849905d6a665ea8c0ca2f2b3fad584202b15
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-02-17 09:41:55 -0300

    Update TextArea.php

--- src/Fields/TextArea.php
+++ src/Fields/TextArea.php
@@ -4,34 +4,36 @@ namespace marcusvbda\vstack\Fields;
 
 class TextArea extends Field
 {
-    public $options = [];
-    public $view = "";
-    public function __construct($op = [])
-    {
-        $this->options = $op;
-        $this->options["type"] = "textarea";
-        parent::processFieldOptions();
-    }
+	public $options = [];
+	public $view = "";
+	public function __construct($op = [])
+	{
+		$this->options = $op;
+		$this->options["type"] = "textarea";
+		parent::processFieldOptions();
+	}
 
-    public function getView()
-    {
-        if (@$this->options["hide"]) return $this->view = "";
+	public function getView()
+	{
+		if (@$this->options["hide"]) return $this->view = "";
 
-        $label          = $this->options["label"];
-        $field          = $this->options["field"];
-        $type           = $this->options["type"];
-        $placeholder    = $this->options["placeholder"];
-        $disabled       = @$this->options["disabled"] ? "true" : "false";
-        $rows           = @$this->options["rows"] ? $this->options["rows"] : 3;
+		$label          = $this->options["label"];
+		$field          = $this->options["field"];
+		$type           = $this->options["type"];
+		$placeholder    = $this->options["placeholder"];
+		$description    = @$this->options["description"];
+		$disabled       = @$this->options["disabled"] ? "true" : "false";
+		$rows           = @$this->options["rows"] ? $this->options["rows"] : 3;
 
-        return $this->view = view("vStack::resources.field.textarea", compact(
-            "disabled",
-            "label",
-            "type",
-            "rows",
-            "placeholder",
-            "visible",
-            "field"
-        ))->render();
-    }
+		return $this->view = view("vStack::resources.field.textarea", compact(
+			"disabled",
+			"label",
+			"type",
+			"rows",
+			"placeholder",
+			"description",
+			"visible",
+			"field"
+		))->render();
+	}
 }
