8293b7a25766680324082552986038a4e42f54d8
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-03-09 12:29:53 -0300

    Update VInput.vue

--- src/Assets/js/components/inputs/VInput.vue
+++ src/Assets/js/components/inputs/VInput.vue
@@ -44,7 +44,7 @@
                     </div>
                     <div class="invalid-feedback" v-if="errors">
                         <ul class="pl-3 mb-0">
-                            <li v-for="(e, i) in errors" :key="i">{{ e }}</li>
+                            <li v-for="(e, i) in errors" :key="i" v-html="e" />
                         </ul>
                     </div>
                 </div>
