ee1d1eaaf0ec6649612cc5ec1cb3c35a1bffd1fa
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-03-12 18:56:32 -0300

    Update view.blade.php

--- src/Views/resources/view.blade.php
+++ src/Views/resources/view.blade.php
@@ -1,66 +1,51 @@
 @extends("templates.admin")
+@php
+	$raw_types = ["Edição" => "edit","Cadastro" => "create"];
+	$current = $data['page_type']." de ".$resource->singularLabel();
+    $current_route = $resource->route()."/".@$content->code."/".$raw_types[$data["page_type"]];
+@endphp
 @section('title',$resource->label())
 @section('breadcrumb')
-<?php 
-    $routes = [];
-    $current = $data['page_type']." de ".$resource->singularLabel();
-    $current_route = $resource->route()."/".@$content->code;
-    Session::push("breadcrumb",[$current=>$current_route]);
-    $bc = Session::get("breadcrumb");
-    Session::forget('breadcrumb');
-	$indexes = [];
-    foreach($bc as $row)
-    {
-        foreach($row as $key=>$value)
-        {
-            if(!in_array($key,$indexes))
-            {
-                $indexes[] = $key;
-                Session::push("breadcrumb",[$key=>$value]);
-                $routes[] = $value;
-            }
-            if($key==$current) {
-                break 2;
-            };
-        }
-    }
-    $bc = Session::get("breadcrumb");
-?>
-
-<div class="row">
-    <div class="col-12">
-        <nav aria-label="breadcrumb">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <?php $bc = Session::get('breadcrumb');?>
-                    @foreach($bc as $rows)
-                        @foreach($rows as $key=>$value)
-                            <li class="breadcrumb-item">
-                                <a  href="{{$value}}" class="link">{{$key}}</a>
-                            </li>
-                        @endforeach
-                    @endforeach
-                </ol>
-            </nav>
-        </nav>
-    </div>
-</div>
+@include("vStack::resources.partials._breadcrumb")
 @endsection
 @section('content')
-    @if(@$resource->beforeViewSlot())
-        {!! @$resource->beforeViewSlot() !!}
-    @endif
-    <resource-view 
-        :data="{{json_encode($data)}}" 
-        :form="{{json_encode($content)}}" 
-        redirect="{{$current_route}}" 
-        :breadcrumb="{{json_encode($routes)}}" 
-    >
-        <template slot="title">
+<div class="row mt-2">
+    <div class="col-12">
+        <div class="d-flex flex-row justify-content-between mb-3">
             <h4>@if( @$resource->icon() ) <span class="{{$resource->icon()}} mr-2"></span> @endif {{$data["page_type"]}} de {{$resource->singularLabel()}}</h4>
-        </template>
-	</resource-view>
-	@if(@$resource->afterViewSlot())
-        {!! @$resource->afterViewSlot() !!}
+        </div>
+    </div>
+</div>
+@if(($data["page_type"]=="Edição") && $resource->useTags())
+	<resource-tags-input resource='{{$resource->id}}' resource_code='{{@$content->code}}'></resource-tags-input>
+@endif
+@if(@!$data->id)
+    @if(@$resource->beforeCreateSlot())
+        {!! @$resource->beforeCreateSlot() !!}
     @endif
+@else 
+    @if(@$resource->beforeEditSlot())
+    {!! @$resource->beforeEditSlot() !!}
+    @endif
+@endif
+<resource-crud 
+    :data="{{json_encode($data)}}"  
+    :params="{{json_encode($params)}}"  
+    redirect="{{$current_route}}" >
+    @if(@!$data->id && !@$data["id"])
+        @if(@$resource->afterCreateSlot())
+            <template slot="aftercreate">
+                {!! @$resource->afterCreateSlot() !!}
+            </template>
+        @endif
+    @else
+        @if(@$resource->afterEditSlot())
+            <template slot="afteredit">
+                {!! @$resource->afterEditSlot() !!}
+            </template>
+        @endif
+    @endif
+</resource-crud>
+
+
 @endsection
