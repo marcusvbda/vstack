7708d1ddf989d7cbc8a35a6c3552204f84270274
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-03-12 18:55:14 -0300

    Update _breadcrumb.blade.php

--- src/Views/resources/partials/_breadcrumb.blade.php
+++ src/Views/resources/partials/_breadcrumb.blade.php
@@ -1,21 +1,24 @@
-<?php 
-    Session::forget('breadcrumb');
-	Session::push('breadcrumb',["Dashboard" => asset('admin')]);
-	Session::push('breadcrumb',[$resource->label() => @$resource->route()]);
-	if($report_mode) Session::push('breadcrumb',['Relatório de '.$resource->label() => @$resource->report_route()]);
-?>
+@php 
+	$bc = [
+		"Dashboard" => "/admin",
+		$resource->label() => @$resource->route()
+	];
+	if(@$report_mode) {
+		$bc['Relatório de '.$resource->label()] = @$resource->report_route();
+	}
+	if(@$data["page_type"]) {
+		$bc[$current] =  $current_route;
+	}
+@endphp
 <div class="row">
     <div class="col-12">
         <nav aria-label="breadcrumb">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <?php $bc = Session::get('breadcrumb');?>
-                    @foreach($bc as $rows)
-                    @foreach($rows as $key=>$value)
-                    <li class="breadcrumb-item">
-                        <a href="{{$value}}" class="link">{{$key}}</a>
-                    </li>
-                    @endforeach
+                    @foreach($bc as $key=>$value)
+						<li class="breadcrumb-item">
+							<a href="{{$value}}" class="link">{{$key}}</a>
+						</li>
                     @endforeach
                 </ol>
             </nav>
