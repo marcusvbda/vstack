04ae3dd566cc7898b8b3e6196f438533d94ccb66
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-03-15 12:29:46 -0300

    Update crud.blade.php

--- src/Views/resources/crud.blade.php
+++ src/Views/resources/crud.blade.php
@@ -1,54 +1,14 @@
 @extends("templates.admin")
 @section('title',$resource->label())
-<?php 
+@php
 $cards = $data["fields"];
+$routes = [];
 $raw_types = ["Edição" => "edit","Cadastro" => "create"];
-
-?>
+$current = $data['page_type']." de ".$resource->singularLabel();
+$current_route = $resource->route()."/".@$content->code."/".$raw_types[$data["page_type"]];
+@endphp
 @section('breadcrumb')
-<?php 
-    $routes = [];
-    $current = $data['page_type']." de ".$resource->singularLabel();
-    $current_route = $resource->route()."/".@$content->code."/".$raw_types[$data["page_type"]];
-    Session::push("breadcrumb",[$current=>$current_route]);
-    $bc = Session::get("breadcrumb");
-    Session::forget('breadcrumb');
-    $indexes = [];
-    foreach($bc as $row)
-    {
-        foreach($row as $key=>$value)
-        {
-            if(!in_array($key,$indexes))
-            {
-                $indexes[] = $key;
-                Session::push("breadcrumb",[$key=>$value]);
-                $routes[] = $value;
-            }
-            if($key==$current) {
-                break 2;
-            };
-        }
-    }
-    $bc = Session::get("breadcrumb");
-?>
-<div class="row">
-    <div class="col-12">
-        <nav aria-label="breadcrumb">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <?php $bc = Session::get('breadcrumb');?>
-                    @foreach($bc as $rows)
-                        @foreach($rows as $key=>$value)
-                            <li class="breadcrumb-item">
-                                <a  href="{{$value}}" class="link">{{$key}}</a>
-                            </li>
-                        @endforeach
-                    @endforeach
-                </ol>
-            </nav>
-        </nav>
-    </div>
-</div>
+@include("vStack::resources.partials._breadcrumb")
 @endsection
 @section('content')
 <div class="row mt-2">
