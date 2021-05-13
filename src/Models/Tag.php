a6c17b5cedaed1b6009276e2142633663e479e27
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-05-13 16:08:58 -0300

    Update Tag.php

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -5,31 +5,31 @@ namespace marcusvbda\vstack\Models;
 use Illuminate\Database\Eloquent\Model;
 use marcusvbda\vstack\Models\Scopes\TenantScope;
 use marcusvbda\vstack\Models\Observers\TenantObserver;
-use marcusvbda\vstack\Traits\CascadeOrRestrictSoftdeletes;
+use marcusvbda\vstack\Models\Traits\CascadeOrRestrictSoftdeletes;
 
 class Tag extends Model
 {
-    use CascadeOrRestrictSoftdeletes;
-    public $table = "resource_tags";
-    public $guarded = ["created_at"];
-    public $cascadeDeletes = ['relation'];
-    public $restrictDeletes = [];
-    public static function boot()
-    {
-        parent::boot();
-        if (static::hasTenant()) {
-            static::observe(new TenantObserver());
-            static::addGlobalScope(new TenantScope());
-        }
-    }
+	use CascadeOrRestrictSoftdeletes;
+	public $table = "resource_tags";
+	public $guarded = ["created_at"];
+	public $cascadeDeletes = ['relation'];
+	public $restrictDeletes = [];
+	public static function boot()
+	{
+		parent::boot();
+		if (static::hasTenant()) {
+			static::observe(new TenantObserver());
+			static::addGlobalScope(new TenantScope());
+		}
+	}
 
-    public static function hasTenant()
-    {
-        return true;
-    }
+	public static function hasTenant()
+	{
+		return true;
+	}
 
-    public function relation()
-    {
-        return $this->hasMany(TagRelation::class, 'resource_tag_id');
-    }
+	public function relation()
+	{
+		return $this->hasMany(TagRelation::class, 'resource_tag_id');
+	}
 }
