98de699a4d4eab130c7c184ffaf3fe9ef2384cf4
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-05-18 19:12:18 -0300

    Update DefaultModel.php

--- src/Models/DefaultModel.php
+++ src/Models/DefaultModel.php
@@ -29,4 +29,36 @@ class DefaultModel extends Model
 	{
 		return true;
 	}
+
+	public function makeTwoLinesHtmlAppend($line, $subline)
+	{
+		return "
+            <div class='d-flex flex-column'>
+                <span><b>{$line}</b></span>
+                <small class='text-muted'>{$subline}</small>
+            </div>
+        ";
+	}
+
+	public function getFCreatedAtBadgeAttribute()
+	{
+		return $this->makeTwoLinesHtmlAppend($this->f_created_at, $this->created_at->diffForHumans());
+	}
+
+	public function getFUpdatedAtBadgeAttribute()
+	{
+		return $this->makeTwoLinesHtmlAppend($this->f_updated_at, $this->updated_at->diffForHumans());
+	}
+
+	public function getFCreatedAtAttribute()
+	{
+		if (!@$this->created_at) return;
+		return $this->created_at->format("d/m/Y - H:i:s");
+	}
+
+	public function getFUpdatedAtAttribute()
+	{
+		if (!@$this->updated_at) return;
+		return $this->updated_at->format("d/m/Y - H:i:s");
+	}
 }
