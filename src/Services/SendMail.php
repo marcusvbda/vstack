d661a69947cfddf8c125e1c17e21261a90bb7cb4
ventuinha <luizpontes@otimize.me>
2021-05-21 11:35:50 -0300

    Add limpeza e validação de email antes do envio

--- src/Services/SendMail.php
+++ src/Services/SendMail.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace marcusvbda\vstack\Services;
 
 use Mail;
@@ -8,16 +9,23 @@ use Illuminate\Support\Facades\Storage;
 class SendMail
 {
 
-	public static function to($to, $subject, $html, $file = null)
-	{
-		Mail::send([], [], function ($message) use ($to, $html, $subject, $file) {
-			$message->to($to)
-				->subject($subject)
-				->setBody($html, 'text/html');
-			if (@$file) {
-				$message->attach(storage_path("app/" . $file));
-			}
-		});
-		Storage::disk("local")->delete($file);
-	}
+    public static function to($to, $subject, $html, $file = null)
+    {
+	$to = filter_var($to,FILTER_SANITIZE_EMAIL);
+        if(!self::validEmail($to)) return;
+
+        Mail::send([], [], function ($message) use ($to, $html, $subject, $file) {
+            $message->to($to)
+                ->subject($subject)
+                ->setBody($html, 'text/html');
+            if (@$file) {
+                $message->attach(storage_path("app/" . $file));
+            }
+        });
+        Storage::disk("local")->delete($file);
+    }
+
+    public static function validEmail($email){
+        return filter_var($email,FILTER_VALIDATE_EMAIL);
+    }
 }
