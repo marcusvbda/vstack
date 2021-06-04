9b7b743fbc6110346d191b85f984f0fa82e447cf
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-06-04 13:16:28 -0300

    Update ResourceController.php

--- src/Controllers/ResourceController.php
+++ src/Controllers/ResourceController.php
@@ -332,6 +332,7 @@ class ResourceController extends Controller
 			}
 		}
 		dispatch(function () use ($user, $resource, $columns, $filter, $file_id, $email, $config, $path, $file_extension) {
+			Auth::loginUsingId($config->data->user_id)
 			$_data = $config->data;
 			$_data->status = "exporting";
 			$config->data = $_data;
