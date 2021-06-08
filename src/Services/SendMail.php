c96c5099c45e8ddbe307aefc691a47ea5a1b336c
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-06-08 14:51:40 -0300

    Update SendMail.php

--- src/Services/SendMail.php
+++ src/Services/SendMail.php
@@ -9,23 +9,27 @@ use Illuminate\Support\Facades\Storage;
 class SendMail
 {
 
-    public static function to($to, $subject, $html, $file = null)
-    {
-	$to = filter_var($to,FILTER_SANITIZE_EMAIL);
-        if(!self::validEmail($to)) return;
+	public static function to($to, $subject, $html, $file = null, $before_send = null)
+	{
+		$to = filter_var($to, FILTER_SANITIZE_EMAIL);
+		if (!self::validEmail($to)) return;
 
-        Mail::send([], [], function ($message) use ($to, $html, $subject, $file) {
-            $message->to($to)
-                ->subject($subject)
-                ->setBody($html, 'text/html');
-            if (@$file) {
-                $message->attach(storage_path("app/" . $file));
-            }
-        });
-        Storage::disk("local")->delete($file);
-    }
+		Mail::send([], [], function ($message) use ($to, $html, $subject, $file) {
+			$message->to($to)
+				->subject($subject)
+				->setBody($html, 'text/html');
+			if (@$file) {
+				$message->attach(storage_path("app/" . $file));
+			}
+		});
+		Storage::disk("local")->delete($file);
+		if ($before_send) {
+			$before_send($to, $subject, $html);
+		}
+	}
 
-    public static function validEmail($email){
-        return filter_var($email,FILTER_VALIDATE_EMAIL);
-    }
+	public static function validEmail($email)
+	{
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
 }
