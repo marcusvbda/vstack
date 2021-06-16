21dfb18fcc785961a72a7f97c137823b3b5e704f
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-06-16 16:16:55 -0300

    Update _default_index.blade.php

--- src/Views/resources/partials/_default_index.blade.php
+++ src/Views/resources/partials/_default_index.blade.php
@@ -146,6 +146,7 @@
         </h4>
         @if($resource->canCreate())
 			<resource-store-btn 
+				class="mt-3"
 				resource_id='{{ $resource->id }}'
 				crud_type='{{ $resource->crudType() }}'
 				label="{{ $resource->storeButtonLabel() }}"
