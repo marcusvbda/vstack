32beb9c666a81c0dd37c2ad4952ae13f27c2f1d9
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-07-19 12:29:48 -0300

    Update VstackController.php

--- src/Controllers/VstackController.php
+++ src/Controllers/VstackController.php
@@ -113,21 +113,21 @@ class VstackController extends Controller
 			if ($filter_type == "or_where_not_in") {
 				$query = $query->where(function ($q) use ($filters) {
 					foreach ($filters as $filter) {
-						$q->orWhereNotIn($filters[0], $filters[1]);
+						$q->orWhereNotIn($filter[0], $filter[1]);
 					}
 				});
 			}
 			if ($filter_type == "where_in") {
 				$query = $query->where(function ($q) use ($filters) {
 					foreach ($filters as $filter) {
-						$q->whereIn($filters[0], $filters[1]);
+						$q->whereIn($filter[0], $filter[1]);
 					}
 				});
 			}
 			if ($filter_type == "where_in") {
 				$query = $query->where(function ($q) use ($filters) {
 					foreach ($filters as $filter) {
-						$q->whereIn($filters[0], $filters[1]);
+						$q->whereIn($filter[0], $filter[1]);
 					}
 				});
 			}
