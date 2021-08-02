eb498cf82c18228a4d6527ae3fe7f246893cea67
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-08-02 16:11:51 -0300

    Update ResourceCrud.vue

--- src/Assets/js/components/resource/ResourceCrud.vue
+++ src/Assets/js/components/resource/ResourceCrud.vue
@@ -57,7 +57,7 @@
 <script>
 import VRuntimeTemplate from 'v-runtime-template'
 export default {
-    props: ['data', 'redirect', 'params', 'raw_type', 'acl', 'first_btn', 'second_btn', 'dialog', 'right_card_content', 'content_id'],
+    props: ['data', 'redirect', 'params', 'raw_type', 'acl', 'first_btn', 'second_btn', 'dialog', 'right_card_content', 'content_id','content'],
     data() {
         return {
             resourceData: {},
