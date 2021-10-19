99fa5ac85157b9685885dd7dbf58bdcb185c2424
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-10-19 11:12:31 -0300

    Update VTags.vue

--- src/Assets/js/components/inputs/VTags.vue
+++ src/Assets/js/components/inputs/VTags.vue
@@ -10,9 +10,9 @@
         </td>
         <td>
             <div class="d-flex flex-column">
-                <div class="d-flex flex-row align-items-center">
+                <div class="d-flex flex-row flex-wrap align-items-center">
                     <el-tag
-                        class="m-2"
+                        class="ml-0 mr-2 mb-2"
                         :key="tag"
                         v-for="tag in dynamicTags"
                         :closable="disabled != true ? true : false"
@@ -24,7 +24,7 @@
                     </el-tag>
                     <template v-if="disabled != true">
                         <el-input
-                            class="input-new-tag ml-0 mr-2"
+                            class="input-new-tag ml-0 mr-2 mb-2"
                             v-if="inputVisible"
                             v-model="inputValue"
                             ref="saveTagInput"
@@ -33,7 +33,15 @@
                             @blur="handleInputConfirm"
                         >
                         </el-input>
-                        <button type="button" v-else class="ml-0 btn btn-primary btn-sm button-new-tag" size="small" @click="showInput">+ Adicionar</button>
+                        <button
+                            type="button"
+                            v-else
+                            class="ml-0 btn btn-primary btn-sm button-new-tag"
+                            size="small"
+                            @click="showInput"
+                        >
+                            + Adicionar
+                        </button>
                     </template>
                 </div>
                 <div class="invalid-feedback" v-if="errors" :style="{ display: `${errors ? 'block' : 'none'}` }">
@@ -48,71 +56,71 @@
 <script>
 export default {
     props: [
-        'placeholder',
-        'label',
-        'route_list',
-        'list_model',
-        'disabled',
-        'errors',
-        'optionlist',
-        'required',
-        'size',
-        'multiple',
-        'relation',
-        'allowcreate',
-        'unique',
-        'description',
-        'extraValidator',
+        "placeholder",
+        "label",
+        "route_list",
+        "list_model",
+        "disabled",
+        "errors",
+        "optionlist",
+        "required",
+        "size",
+        "multiple",
+        "relation",
+        "allowcreate",
+        "unique",
+        "description",
+        "extraValidator"
     ],
     data() {
         return {
             dynamicTags: [],
             inputVisible: false,
             started: false,
-            inputValue: '',
-        }
+            inputValue: ""
+        };
     },
     watch: {
         dynamicTags() {
             if (this.started) {
-                return this.$emit('input', val)
+                return this.$emit("input", val);
             }
-        },
+        }
     },
     mounted() {
-        this.dynamicTags = this.$attrs.value ? this.$attrs.value : []
+        this.dynamicTags = this.$attrs.value ? this.$attrs.value : [];
     },
     methods: {
         handleClose(tag) {
-            this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+            this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
         },
 
         showInput() {
-            this.inputVisible = true
-            this.$nextTick((_) => {
-                this.$refs.saveTagInput.$refs.input.focus()
-            })
+            this.inputVisible = true;
+            this.$nextTick(() => {
+                this.$refs.saveTagInput.$refs.input.focus();
+            });
         },
 
         handleInputConfirm() {
-            if (!this.inputVisible) return
+            if (!this.inputVisible) return;
             if (this.unique) {
-                if (this.dynamicTags.find((x) => x == this.inputValue))
-                    return this.$message({ showClose: true, message: `${this.inputValue} já existe`, type: 'error' })
+                if (this.dynamicTags.find(x => x == this.inputValue))
+                    return this.$message({ showClose: true, message: `${this.inputValue} já existe`, type: "error" });
             }
-            let inputValue = this.inputValue
+            let inputValue = this.inputValue;
             if (inputValue) {
                 if (this.extraValidator) {
-                    let valid = this.extraValidator.handle(inputValue)
-                    if (!valid) return this.$message({ showClose: true, message: this.extraValidator.message, type: 'error' })
+                    let valid = this.extraValidator.handle(inputValue);
+                    if (!valid) return this.$message({ showClose: true, message: this.extraValidator.message, type: "error" });
                 }
-                this.dynamicTags.push(inputValue)
+                this.dynamicTags.push(inputValue);
             }
-            this.inputVisible = false
-            this.inputValue = ''
-        },
-    },
-}
+            this.inputVisible = false;
+            this.inputValue = "";
+        }
+    }
+};
 </script>
 <style>
 .el-tag + .el-tag {
@@ -122,11 +130,10 @@ export default {
     margin-left: 10px;
     height: 32px;
     line-height: 30px;
-    padding-top: 0;
-    padding-bottom: 0;
+    padding: 0 40px;
 }
 .input-new-tag {
-    width: 90px;
+    width: 160px;
     margin-left: 10px;
     vertical-align: bottom;
 }
