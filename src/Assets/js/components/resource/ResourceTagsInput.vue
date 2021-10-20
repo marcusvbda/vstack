9f66adee798e8fc4f72825cf53d98a9d483be9e4
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-10-20 16:46:29 -0300

    Update ResourceTagsInput.vue

--- src/Assets/js/components/resource/ResourceTagsInput.vue
+++ src/Assets/js/components/resource/ResourceTagsInput.vue
@@ -2,7 +2,7 @@
     <div class="row mb-3">
         <transition name="fade">
             <div class="col-12 d-flex flex-row flex-wrap align-items-center" v-if="!loading">
-                <el-tag
+                <ElTag
                     :style="{ '--color': t.color }"
                     class="resource-tag"
                     size="mini"
@@ -12,15 +12,16 @@
                     color="transparent"
                     :hit="true"
                     @close="handleClose(t, i)"
-                    >{{ t.name }}</el-tag
                 >
+                    {{ t.name }}
+                </ElTag>
                 <button class="ml-2 btn-new-tag" title="Nova Tag" @click="dialogVisible = true" v-if="only_view == undefined">
                     <span class="el-icon-price-tag" />
                 </button>
-                <el-dialog title="Adicionar Tags" :visible.sync="dialogVisible" width="30%">
+                <ElDialog title="Adicionar Tags" :visible.sync="dialogVisible" width="30%">
                     <div class="row">
                         <div class="col-12">
-                            <el-select
+                            <ElSelect
                                 class="w-100"
                                 v-model="newTag"
                                 filterable
@@ -28,25 +29,30 @@
                                 default-first-option
                                 placeholder="Selecione ou Digite a tag que deseja adicionar"
                             >
-                                <el-option v-for="t in filteredOption" :key="t.id" :label="t.name" :value="t.name">
-                                    <el-tag
+                                <ElOption v-for="t in filteredOption" :key="t.id" :label="t.name" :value="t.name">
+                                    <ElTag
                                         :style="{ borderColor: t.color, color: t.color, fontWeight: 700 }"
                                         class="resource-tag"
                                         size="mini"
                                         :key="t.id"
                                         color="transparent"
                                         :hit="true"
-                                        >{{ t.name }}</el-tag
                                     >
-                                </el-option>
-                            </el-select>
+                                        {{ t.name }}
+                                    </ElTag>
+                                </ElOption>
+                            </ElSelect>
                         </div>
                     </div>
                     <span slot="footer" class="dialog-footer" v-if="canSave">
-                        <el-button @click="dialogVisible = false">Cancelar</el-button>
-                        <el-button type="primary" @click="addTag" v-loading="submiting" :disabled="submiting">Adicionar</el-button>
+                        <ElButton @click="dialogVisible = false">
+                            Cancelar
+                        </ElButton>
+                        <ElButton type="primary" @click="addTag" v-loading="submiting" :disabled="submiting">
+                            Adicionar
+                        </ElButton>
                     </span>
-                </el-dialog>
+                </ElDialog>
             </div>
         </transition>
     </div>
@@ -54,103 +60,96 @@
 
 <script>
 export default {
-    props: ['resource', 'resource_code', 'only_view'],
+    props: ["resource", "resource_code", "only_view", "default_tags", "default_options"],
     data() {
         return {
             dialogVisible: false,
-            newTag: '',
+            newTag: "",
             attempts: 0,
             attempt_options: 0,
             loading: true,
             loading_options: true,
             tags: [],
             options: [],
-            submiting: false,
-        }
+            submiting: false
+        };
     },
     computed: {
         filteredOption() {
-            return this.options.filter((x) => !this.tags.map((x) => x.id).includes(x.id))
+            return this.options.filter(x => !this.tags.map(x => x.id).includes(x.id));
         },
         canSave() {
-            if (!this.newTag) return false
-            if (this.tags.filter((x) => x.name == this.newTag).length > 0) return false
-            return true
-        },
+            if (!this.newTag) return false;
+            if (this.tags.filter(x => x.name == this.newTag).length > 0) return false;
+            return true;
+        }
     },
     created() {
-        this.getTags()
-        if (this.only_view == undefined) return this.getOptions()
+        if (Array.isArray(this.default_tags)) {
+            this.tags = this.default_tags;
+            this.loading_options = false;
+            this.loading = false;
+        } else {
+            this.getTags();
+        }
+        if (this.only_view == undefined) {
+            if (Array.isArray(this.default_options)) {
+                this.options = this.default_options;
+                this.loading_options = false;
+            } else {
+                this.getOptions();
+            }
+        }
     },
     methods: {
-        addTag() {
-            this.dialogVisible = false
-            this.$nextTick(() => (this.newTag = ''))
-        },
         handleClose(tag, index) {
-            this.$confirm('Remover tag ?', 'Confirmação', {
-                confirmButtonText: 'Sim',
-                cancelButtonText: 'Não',
-                type: 'warning',
+            this.$confirm("Remover tag ?", "Confirmação", {
+                confirmButtonText: "Sim",
+                cancelButtonText: "Não",
+                type: "warning"
             }).then(() => {
                 this.$http.delete(`/admin/${this.resource}/${this.resource_code}/tags/destroy/${tag.id}`).then(() => {
-                    this.tags.splice(index, 1)
-                })
-            })
+                    this.tags.splice(index, 1);
+                });
+            });
         },
         getTags() {
-            this.attempts++
-            this.$http
-                .get(`/admin/${this.resource}/${this.resource_code}/tags`)
-                .then((resp) => {
-                    resp = resp.data
-                    this.tags = resp
-                    this.loading = false
-                })
-                .catch((er) => {
-                    if (this.attempts <= 3) return this.getTags()
-                    this.loading = false
-                    console.log(er)
-                })
+            this.attempts++;
+            this.$http.get(`/admin/${this.resource}/${this.resource_code}/tags`, {}, { retries: 3 }).then(({ data }) => {
+                this.tags = data;
+                this.loading = false;
+            });
         },
         getOptions() {
-            this.attempt_options++
-            this.$http
-                .get(`/admin/${this.resource}/tags/options`)
-                .then((resp) => {
-                    resp = resp.data
-                    this.options = resp
-                    this.loading_options = false
-                })
-                .catch((er) => {
-                    if (this.attempt_options <= 3) return this.getTags()
-                    this.loading_options = false
-                    console.log(er)
-                })
+            this.attempt_options++;
+            this.$http.get(`/admin/${this.resource}/tags/options`, {}, { retries: 3 }).then(({ data }) => {
+                this.options = data;
+                this.loading_options = false;
+            });
         },
         addTag() {
-            this.submiting = true
+            this.submiting = true;
             this.$http
                 .post(`/admin/${this.resource}/${this.resource_code}/tags/add`, {
-                    name: this.newTag,
+                    name: this.newTag
                 })
-                .then((resp) => {
-                    resp = resp.data
-                    this.tags.push(resp)
-                    this.loading = false
-                    this.dialogVisible = false
+                .then(resp => {
+                    resp = resp.data;
+                    this.tags.push(resp);
+                    this.loading = false;
+                    this.dialogVisible = false;
                     this.$nextTick(() => {
-                        this.newTag = ''
-                        this.submiting = false
-                    })
+                        this.newTag = "";
+                        this.submiting = false;
+                    });
                 })
-                .catch((er) => {
-                    this.loading = false
-                    console.log(er)
-                })
-        },
-    },
-}
+                .catch(er => {
+                    this.loading = false;
+                    console.log(er);
+                });
+        }
+    }
+};
 </script>
 <style lang="scss" scoped>
 .btn-new-tag {
