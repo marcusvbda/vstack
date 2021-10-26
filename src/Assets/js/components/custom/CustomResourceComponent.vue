c1a5fc6a75993d4c1997f315fbd6f5517523bd4e
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-10-26 12:26:52 -0300

    Create CustomResourceComponent.vue

---
+++ src/Assets/js/components/custom/CustomResourceComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <tr>
+        <td class="w-25">
+            <div class="d-flex flex-column">
+                <b class="input-title" v-if="label" v-html="label" />
+                <small class="text-muted mt-1" v-if="description" v-html="description" />
+            </div>
+        </td>
+        <td :class="custom_class">
+            <slot />
+        </td>
+    </tr>
+</template>
+<script>
+export default {
+    props: ["label", "description", "custom_class"]
+};
+</script>
