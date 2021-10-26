2ebb8fe3fe663c8757bab4beb5abb7af6bf4144a
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-10-26 11:37:59 -0300

    Update VRadio.vue

--- src/Assets/js/components/inputs/VRadio.vue
+++ src/Assets/js/components/inputs/VRadio.vue
@@ -11,7 +11,12 @@
         <td>
             <div class="d-flex flex-column">
                 <el-radio-group :disabled="disabled" v-model="val" v-bind:class="{ 'is-invalid': errors }">
-                    <el-radio-button v-for="(op, i) in option_list" :key="i" :label="op.value ? op.value : op">
+                    <el-radio-button
+                        v-for="(op, i) in option_list"
+                        :key="i"
+                        :label="op.value ? op.value : op"
+                        v-bind:class="{ 'option-selected': val == (op.value ? op.value : op) }"
+                    >
                         <div v-html="op.label ? op.label : op" />
                     </el-radio-button>
                 </el-radio-group>
@@ -26,19 +31,19 @@
 </template>
 <script>
 export default {
-    props: ['label', 'errors', 'disabled', 'option_list', 'description'],
+    props: ["label", "errors", "disabled", "option_list", "description"],
     data() {
         return {
-            val: null,
-        }
+            val: null
+        };
     },
     watch: {
         val(val) {
-            return this.$emit('input', val)
-        },
+            return this.$emit("input", val);
+        }
     },
     async created() {
-        this.val = this.$attrs.value
-    },
-}
+        this.val = this.$attrs.value;
+    }
+};
 </script>
