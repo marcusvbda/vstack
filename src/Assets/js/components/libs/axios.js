2a0abdb9b07654316788a36705696c8f38f1f915
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-10-29 10:17:54 -0300

    Update axios.js

--- src/Assets/js/components/libs/axios.js
+++ src/Assets/js/components/libs/axios.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import axios from 'axios'
-const axiosRetry = require('axios-retry')
+import Vue from "vue";
+import axios from "axios";
+import axiosRetry from "axios-retry";
 axiosRetry(axios, {
-	retries: 0,
-	shouldResetTimeout: true,
-	retryCondition: (_error) => true
-})
-Vue.prototype.$http = axios
+    retries: 0,
+    shouldResetTimeout: true,
+    retryCondition: () => true,
+});
+Vue.prototype.$http = axios;
