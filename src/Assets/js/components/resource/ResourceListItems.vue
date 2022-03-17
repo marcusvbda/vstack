1703ec2809cb48e5931f3587bede2556f99b7d19
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-03-17 18:16:36 -0300

    Update ResourceListItems.vue

--- src/Assets/js/components/resource/ResourceListItems.vue
+++ src/Assets/js/components/resource/ResourceListItems.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="row list-items">
         <div v-for="(list_item, i) in list_items" :key="i" :class="`col list-item ${list_item.class ? list_item.class : ''}`">
-            <div class="list-item--content">
+            <v-runtime-template v-if="list_item.template" :key="i" :template="list_item.template" />
+            <div class="list-item--content" v-else>
                 <b class="list-item--title" v-html="list_item.label ? list_item.label : ''" />
                 <div class="list-item--value text-muted" v-html="list_item.value ? list_item.value : ''" />
             </div>
@@ -9,7 +10,12 @@
     </div>
 </template>
 <script>
+import VRuntimeTemplate from "v-runtime-template";
+
 export default {
     props: ["list_items"],
+    components: {
+        "v-runtime-template": VRuntimeTemplate
+    },
 };
 </script>
