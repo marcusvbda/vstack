c76269037c7e63252b28d2adb07392a69b9cdbdc
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-03-23 16:49:51 -0300

    Update autoload.js

--- src/Assets/js/components/autoload.js
+++ src/Assets/js/components/autoload.js
@@ -1,14 +1,6 @@
 import Vue from "vue";
 const files = require.context("./", true, /(\/)(?!.*\/)(?!-.*$).*\.vue$/i);
-files.keys().map((key) =>
-  Vue.component(
-    key
-      .split("/")
-      .pop()
-      .split(".")[0],
-    files(key).default
-  )
-);
+files.keys().map((key) => Vue.component(key.split("/").pop().split(".")[0], files(key).default));
 import moment from "moment";
 require("./libs/charts");
 require("./libs/cookies");
@@ -36,6 +28,8 @@ Vue.config.devtools = true;
 Vue.prototype.$moment = moment;
 const debug = require("console-development");
 Vue.prototype.$debug = debug;
+import io from "socket.io-client";
+Vue.prototype.$io = io;
 import PortalVue from "portal-vue";
 Vue.use(PortalVue);
 import AOS from "aos";
@@ -44,81 +38,78 @@ import getDefaultStore from "../../store";
 
 Vue.prototype.$aos = AOS;
 const vue_settings = {
-  store: null,
-  data() {
-    return {
-      root_loading: false,
-    };
-  },
-  created() {
-    this.init();
-    this.$pace.start();
-  },
-  mounted() {
-    this.$pace.stop();
-  },
-  methods: {
-    init() {
-      this.$aos.init({
-        once: true,
-        disable: !laravel.vstack.animation_enabled,
-        duration: 200,
-      });
-      let body = document.querySelector("body");
-      body.style.display = "block";
-
-      if (laravel.user) {
-        if (laravel.user.code) {
-          this.$http
-            .post(
-              `${laravel.general.root_url}/admin/vstack/notifications/${laravel.user.code}`,
-              {}
-            )
-            .then((res) => {
-              res = res.data;
-              res.notifications
-                .filter((not) => not.alert_type == "vstack_alert")
-                .map((not) => {
-                  setTimeout(() => {
-                    this.$message({
-                      showClose: true,
-                      message: not.data.message,
-                      type: not.data.type,
-                    });
-                  });
-                });
-            });
-        }
-      }
+    store: null,
+    data() {
+        return {
+            root_loading: false,
+        };
     },
-  },
+    created() {
+        this.init();
+        this.$pace.start();
+    },
+    mounted() {
+        this.$pace.stop();
+    },
+    methods: {
+        init() {
+            this.$aos.init({
+                once: true,
+                disable: !laravel.vstack.animation_enabled,
+                duration: 200,
+            });
+            let body = document.querySelector("body");
+            body.style.display = "block";
+
+            if (laravel.user) {
+                if (laravel.user.code) {
+                    this.$http
+                        .post(`${laravel.general.root_url}/admin/vstack/notifications/${laravel.user.code}`, {})
+                        .then((res) => {
+                            res = res.data;
+                            res.notifications
+                                .filter((not) => not.alert_type == "vstack_alert")
+                                .map((not) => {
+                                    setTimeout(() => {
+                                        this.$message({
+                                            showClose: true,
+                                            message: not.data.message,
+                                            type: not.data.type,
+                                        });
+                                    });
+                                });
+                        });
+                }
+            }
+        },
+    },
 };
 import Vuex from "vuex";
 Vue.use(Vuex);
 
 window.VueApp = {
-  div: "#app",
-  settings: vue_settings,
-  app: null,
-  store_modules: getDefaultStore(),
-  setStore(newStore) {
-    this.store = newStore;
-  },
-  setSettings(newSettings) {
-    this.settings = newSettings;
-  },
-  setStoreModules(newModules) {
-    this.store_modules = newModules;
-  },
-  appendStoreModule(moduleName, newModule) {
-    this.store_modules.modules[moduleName] = newModule;
-  },
-  initStore() {
-    this.settings.store = new Vuex.Store(this.store_modules);
-  },
-  start() {
-    this.initStore();
-    this.app = new Vue(this.settings);
-    this.app.$mount(this.div);
-  },
+    div: "#app",
+    settings: vue_settings,
+    app: null,
+    store_modules: getDefaultStore(),
+    setStore(newStore) {
+        this.store = newStore;
+    },
+    setSettings(newSettings) {
+        this.settings = newSettings;
+    },
+    setStoreModules(newModules) {
+        this.store_modules = newModules;
+    },
+    appendStoreModule(moduleName, newModule) {
+        this.store_modules.modules[moduleName] = newModule;
+    },
+    initStore() {
+        this.settings.store = new Vuex.Store(this.store_modules);
+    },
+    start() {
+        this.initStore();
+        this.app = new Vue(this.settings);
+        this.app.$mount(this.div);
+    },
 };
