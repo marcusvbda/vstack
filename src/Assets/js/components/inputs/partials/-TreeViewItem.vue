64a0af181c1f818a2213c751af5c90d8be0dbe1a
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-04-19 20:19:34 -0300

    Update -TreeViewItem.vue

--- src/Assets/js/components/inputs/partials/-TreeViewItem.vue
+++ src/Assets/js/components/inputs/partials/-TreeViewItem.vue
@@ -1,19 +1,32 @@
 <template>
     <div :class="['tree-view-item group', resource]">
-        <div :class="['tree-view-label hoverable gray', visible && 'opened']" @click="visible = !visible">
-            <i :class="['icon', visible ? 'el-icon-caret-bottom' : 'icon el-icon-caret-right']" />
-            {{ label }}
+        <div :class="['tree-view-label hoverable gray', visible && 'opened']">
+            <div class="w-75" @click="visible = !visible">
+                <i :class="['icon', visible ? 'el-icon-caret-bottom' : 'icon el-icon-caret-right']" />
+                {{ label }}
+            </div>
+            <div class="ml-auto w-25">
+                <el-input
+                    clearable
+                    :placeholder="`Encontrar  ${singular_label} ...`"
+                    prefix-icon="el-icon-search"
+                    v-model="filter"
+                    size="mini"
+                />
+            </div>
         </div>
         <div class="tree-view-children" v-if="visible"><slot /></div>
     </div>
 </template>
 <script>
 export default {
-    props: ["label", "default_visible", "resource"],
+    props: ["label", "default_visible", "resource", "singular_label"],
     data() {
         return {
             visible: this.default_visible ? true : false,
             loading: true,
+            filter: "",
+            interval: null,
         };
     },
     watch: {
@@ -21,6 +34,13 @@ export default {
             if (val) {
                 this.$emit("opened");
             }
+        },
+        filter(val) {
+            clearInterval(this.interval);
+            this.interval = setInterval(() => {
+                this.$emit("filter-changed", val);
+                clearInterval(this.interval);
+            }, 400);
         },
     },
     created() {
