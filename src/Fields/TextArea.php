e98ba0b09ad0c2b332651bc81b72e5d6fc47bebf
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-05-04 17:19:26 -0300

    Update TextArea.php

--- src/Fields/TextArea.php
+++ src/Fields/TextArea.php
@@ -31,7 +31,7 @@ class TextArea extends Field
 		$rows           = @$this->options["rows"] ? $this->options["rows"] : 3;
 		$maxlength      = $this->options["maxlength"] ?  $this->options["maxlength"] : $this->getDefaultMaxlength(9999999999);
 		$eval = " " . (@$this->options["eval"] ? trim($this->options["eval"]) : "") . " ";
-		$show_value_length = (@$this->options["show_value_length"] !== null) ? $this->options["show_value_length"] : in_array($type,["text","textarea"]);
+		$show_value_length = (@$this->options["show_value_length"] !== null) ? $this->options["show_value_length"] : "false";
 		$show_value_length = $show_value_length ? 'true' : 'false';
 
 		return $this->view = view("vStack::resources.field.textarea", compact(
