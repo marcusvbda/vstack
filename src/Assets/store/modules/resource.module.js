10945966188e2d6d84f87f0ce98a0a7a7f56af31
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-08-26 18:57:49 -0300

    Update resource.module.js

--- src/Assets/store/modules/resource.module.js
+++ src/Assets/store/modules/resource.module.js
@@ -1,14 +1,19 @@
 const state = {
     action_btn_loading: false,
+    inputs_initialized: [],
 };
 
 const getters = {
     action_btn_loading: (state) => state.action_btn_loading,
+    inputs_initialized: (state) => state.inputs_initialized,
 };
 
 const mutations = {
     setActionBtnLoading: (state, payload) => {
         state.action_btn_loading = payload;
+    },
+    pushInputInitialized: (state, payload) => {
+        state.inputs_initialized.push(payload);
     },
 };
 
