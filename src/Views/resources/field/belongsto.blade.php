2fa749eeeb7a624dc257c6363608ab8953bfd9c8
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-09-01 11:51:16 -0300

    Update belongsto.blade.php

--- src/Views/resources/field/belongsto.blade.php
+++ src/Views/resources/field/belongsto.blade.php
@@ -8,9 +8,10 @@
     :multiple='@json($multiple)' 
     route_list='{{$route_list}}' 
     field_index="{{$field}}"
-    :option_list='{{$options}}'  
+    :option_list='{{$options}}' 
+    :allow_create='{{$allow_create}}'    
     :model_fields='@json($model_fields)'
-	all_options_label="{{ $all_options_label }}"
+	all_options_label="{{ $all_options_label }}"    
 	list_model='{{$model}}'
 	:errors='{{"errors.$field ? errors.$field : false"}}' 
     id="resource-input-belongsto-{{ $field }}" 
