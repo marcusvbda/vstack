62a1b89ada38255a7f39663dd559f456ed99ddcf
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-09-14 20:19:09 -0300

    Update data_table.blade.php

--- src/Views/resources/loader/data_table.blade.php
+++ src/Views/resources/loader/data_table.blade.php
@@ -1,19 +1,10 @@
 <resource-list-items resource_id="{{ $resource->id }}" :request_data='@json(request()->all())'></resource-list-items>   
 <div class="row">
     <div class="col-12">
-        @if ($data->total() > 0)
-            @if ($report_mode)
-                @include("vStack::resources.partials._report_table")
-            @else
-                @include("vStack::resources.partials._table")
-            @endif
+        @if ($report_mode)
+            @include("vStack::resources.partials._report_table")
         @else
-            @if ($resource->lenses())
-                @include("vStack::resources.partials._lenses")
-            @endif
-            <h4 class="text-center my-4">
-                {{ $resource->noResultsFoundText() }}
-            </h4>
+            @include("vStack::resources.partials._table")
         @endif
     </div>
 </div> 
