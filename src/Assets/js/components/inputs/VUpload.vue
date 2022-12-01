a8b62e3e9fb5339aca15a6ffd8ef5b9d990da1cf
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2022-12-01 18:37:53 -0300

    Update VUpload.vue

--- src/Assets/js/components/inputs/VUpload.vue
+++ src/Assets/js/components/inputs/VUpload.vue
@@ -74,7 +74,7 @@ export default {
     props: {
         aspect_ratio: {
             type: Number,
-            default: 1240 / 310
+            default: 1
         },
         crop_image: {
             type: Boolean,
