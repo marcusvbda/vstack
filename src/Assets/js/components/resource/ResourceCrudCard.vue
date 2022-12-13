157fe3209a5ff74d6dd48f1cb58b87b8b5e8fd21
mvbassalobre <bassalobre.vinicius@gmail.com>
2022-12-13 14:02:55 -0300

    resource card

--- src/Assets/js/components/resource/ResourceCrudCard.vue
+++ src/Assets/js/components/resource/ResourceCrudCard.vue
@@ -2,6 +2,7 @@
     <div :id="label" :index="index">
         <div class="card crud-card mb-4 p-0">
             <CrudCardHeader :label="label" :description="description">
+                <VRuntimeTemplate v-if="append_content" :template="append_content" />
                 <div class="d-flex justify-content-end" v-if="advanced">
                     <el-checkbox v-model="showing" class="mb-1">Visualizar Informações</el-checkbox>
                 </div>
@@ -17,11 +18,16 @@
     </div>
 </template>
 <script>
+import VRuntimeTemplate from "v-runtime-template";
+
 export default {
-    props: ["label", "description", "advanced", "inputs", "index"],
+    props: ["label", "description", "advanced", "inputs", "index", "append_content", "form"],
     data: () => ({
         showing: false
     }),
+    components: {
+        VRuntimeTemplate
+    },
     computed: {
         real_showing() {
             if (!this.advanced) return true;
--- src/Views/resources/field/card.blade.php
+++ src/Views/resources/field/card.blade.php
@@ -1,4 +1,4 @@
-<resource-crud-card label="{{ @$label }}" description="{{ @$description }} ":advanced="{{ json_encode(@$advanced ? true : false) }}" index="{{ $index }}" {!! $eval !!} >
+<resource-crud-card :form="form" append_content='{{@$append_content}}' label="{{ @$label }}" description="{{ @$description }} ":advanced="{{ json_encode(@$advanced ? true : false) }}" index="{{ $index }}" {!! $eval !!} >
     <div class="row-responsive-table">
         <table class="table table-crud mb-0">
             <tbody>{!! $inputs !!}</tbody>
