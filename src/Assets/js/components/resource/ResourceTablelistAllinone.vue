c00475260bddceded5e124378ebad372c5bd5e2e
mvbassalobre <bassalobre.vinicius@gmail.com>
2023-01-27 17:27:52 -0300

    corrige afterslot

--- src/Assets/js/components/resource/ResourceTablelistAllinone.vue
+++ src/Assets/js/components/resource/ResourceTablelistAllinone.vue
@@ -3,45 +3,30 @@
         <template v-for="(row, i) in rows">
             <tr :key="i" :id="`resource-body-${row.content.id}`">
                 <td v-for="(col, y) in columns" :key="`${i}_${y}`" :id="`resource-body-${col}`">
-                    <resource-tablelist-allinone-item
-                        :virtual_indexes="virtual_indexes"
-                        :row="row"
-                        :col="col"
-                        :first_key="first_key == col"
-                        :resource_id="resource_id"
-                        :resource_route="resource_route"
-                    >
-                        <template v-if="table_after_row" v-slot:after_row="{ after_row, after_row_content }">
-                            <el-tooltip
-                                class="item"
-                                effect="dark"
-                                :content="`${after_row.visible ? 'Ver Menos' : 'Ver Mais'}`"
-                                placement="top"
-                            >
-                                <span
-                                    class="el-icon-arrow-down arrow-after-icon"
-                                    v-if="!after_row.visible"
-                                    @click="after_row.visible = true"
-                                />
-                                <span class="el-icon-arrow-up arrow-after-icon" v-else @click="after_row.visible = false" />
-                            </el-tooltip>
-                            <portal :to="`resource_after_row_arrow_${row.content.id}`">
-                                <transition name="fade">
-                                    <div class="container-fluid content py-3" v-if="after_row.visible">
-                                        <v-runtime-template :template="after_row_content" />
-                                    </div>
-                                </transition>
-                            </portal>
+                    <resource-tablelist-allinone-item :virtual_indexes="virtual_indexes" :row="row" :col="col"
+                        :first_key="first_key == col" :resource_id="resource_id" :resource_route="resource_route">
+                        <template v-slot:after_row="{ after_row, after_row_content }">
+                            <template v-if="row.content.after_row">
+                                <el-tooltip class="item" effect="dark"
+                                    :content="`${after_row.visible ? 'Ver Menos' : 'Ver Mais'}`" placement="top">
+                                    <span class="el-icon-arrow-down arrow-after-icon" v-if="!after_row.visible"
+                                        @click="after_row.visible = true" />
+                                    <span class="el-icon-arrow-up arrow-after-icon" v-else
+                                        @click="after_row.visible = false" />
+                                </el-tooltip>
+                                <portal :to="`resource_after_row_arrow_${row.content.id}`">
+                                    <transition name="fade">
+                                        <div class="container-fluid content py-3" v-if="after_row.visible">
+                                            <v-runtime-template :template="after_row_content" />
+                                        </div>
+                                    </transition>
+                                </portal>
+                            </template>
                         </template>
                     </resource-tablelist-allinone-item>
                 </td>
             </tr>
-            <tr
-                class="table-row after"
-                v-if="table_after_row"
-                :key="`table_after_row_${i}`"
-                :id="`resource-body-${row.content.id}-after-row`"
-            >
+            <tr class="table-row after" :key="`table_after_row_${i}`" :id="`resource-body-${row.content.id}-after-row`">
                 <td :colspan="columns.length">
                     <portal-target :key="`portal_${i}`" :name="`resource_after_row_arrow_${row.content.id}`" />
                     <div class="spacer" />
@@ -53,7 +38,7 @@
 <script>
 import VRuntimeTemplate from "v-runtime-template";
 export default {
-    props: ["rows", "table_keys", "table_after_row", "has_actions", "show_right_actions_column", "resource_id", "resource_route"],
+    props: ["rows", "table_keys", "has_actions", "show_right_actions_column", "resource_id", "resource_route"],
     data() {
         return {
             first_key: this.table_keys[0],
@@ -74,9 +59,7 @@ export default {
         },
         columns() {
             let columns = [];
-            if (this.table_after_row) {
-                columns.push(this.virtual_indexes.after_row);
-            }
+            columns.push(this.virtual_indexes.after_row);
             if (this.has_actions) {
                 columns.push(this.virtual_indexes.action_col);
             }
