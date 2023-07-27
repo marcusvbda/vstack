311ce907f4d426c1bf80de6f0574679fe182e839
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-07-26 23:44:29 -0300

    alert

---
+++ src/Assets/js/components/custom/ShowAlert.vue
@@ -0,0 +1,19 @@
+<template></template>
+<script>
+export default {
+    props: ['messages'],
+    created() {
+        let showAbleMessages = this.messages.filter(
+            (row) =>
+                typeof row?.message == 'string' && typeof row?.type == 'string'
+        );
+        let lastMessage = showAbleMessages[showAbleMessages.length - 1];
+        this.$message({
+            dangerouslyUseHTMLString: true,
+            showClose: true,
+            message: lastMessage.message,
+            type: lastMessage.type,
+        });
+    },
+};
+</script>
